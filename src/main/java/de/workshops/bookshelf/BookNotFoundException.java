363bf7fcba27cb200dc93c76930a5f55789e7eee
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-10-05 14:06:23 +0200

    Sample solution for day 2

---
+++ src/main/java/de/workshops/bookshelf/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package de.workshops.bookshelf;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Book not found", code = HttpStatus.I_AM_A_TEAPOT)
+public class BookNotFoundException extends Exception {
+}
